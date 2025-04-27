75f851d8655a6426f08bba03158d653e045fa469
Séjœ <jmmwang800@gmail.com>
2025-04-27 13:13:59 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -213,7 +213,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 *├⬡* *.ʀᴀɴᴋ*
 *├⬡* *.Tɪᴍᴇᴢᴏɴᴇ*
 *├⬡* *.ᴅᴇꜰɪɴᴇ*
-*├🫠* *.Dᴀɪʟʏꜰᴀᴄᴛ*
+*├🌞* *.Dᴀɪʟʏꜰᴀᴄᴛ*
 *┕─────────────❒*
 > ᴘᴏᴡᴇʀᴇᴅ ʙʏ JOE🎲`;
 
